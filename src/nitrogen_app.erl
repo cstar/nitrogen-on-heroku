8b89675818ee807bfd951c3c8b325cac05b17388
Eric Cestari <ecestari@mac.com>
2015-10-14 16:12:54 +0200

    update to latest nitrogen

--- src/nitrogen_app.erl
+++ src/nitrogen_app.erl
@@ -1,15 +1,23 @@
 -module(nitrogen_app).
 -behaviour(application).
--export([start/0,start/2, stop/1]).
-
+-export([start/2, stop/1]).
+-export([start/0]).
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+
 start()->
-  application:start(nitrogen).
+  application:ensure_all_started(nitrogen).
 
 start(_StartType, _StartArgs) ->
+  case os:getenv("PORT") of
+    false ->
+      ok;
+    P ->
+        {Pi, _} = string:to_integer(P),
+        application:set_env(simple_bridge, port, Pi)
+  end,
     nitrogen_sup:start_link().
 
 stop(_State) ->
